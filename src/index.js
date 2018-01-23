cf4473fb3eb2835afe017bbbbe4664be0ae4b165
Bilo Lwabona <bilo.lwabona@gmail.com>
2018-01-23 19:45:35 +0200

    added highlight.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 // import { Icon } from 'bilo-ui'
 import './style.scss'
-import Highlight from 'react-highlight.js'
+// import Highlight from 'react-highlight.js'
+// import Highlight from 'react-highlight'
+import Highlight from 'react-hljs'
 import jsxToString from 'jsx-to-string-2'
+var hljs = require('highlight.js')
+hljs.initHighlightingOnLoad()
 
 class CodeDemo extends React.Component {
     state = {
         isOpen: true
     }
 
-    componentDidMount() {
+    componentWillMount() {
         const { code, title, functionNameOnly, useFunctionCode } = this.props;
         console.log(title, code)
+        console.log('hljs', hljs.highlight('html','<div></div>'))
         this.setState({
             codeString: jsxToString(code, {
                 // functionNameOnly: true,
@@ -43,9 +48,10 @@ class CodeDemo extends React.Component {
                     <div className='code-block'>
                         {
                             code && isOpen
-                            ? <Highlight language={'html'}>
-                                {codeString ? codeString : ''}
-                            </Highlight> 
+                            // ? <Highlight className={'html'}>
+                            //     {codeString ? codeString : '<div>...loading code</div>'}
+                            //     </Highlight>
+                                ? <span dangerouslySetInnerHTML={{ __html: `${hljs.highlight('html', codeString).value}` }}></span>    
                             :null
                         }
                     </div>
